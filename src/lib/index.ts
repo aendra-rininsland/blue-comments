d0ca48762f57de3370aab4d4d57f464e7b714c31
Ændra Rininsland <aendra.rininsland@ft.com>
2024-07-02 02:49:39 +0100

    paths

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,2 @@
 // Reexport your entry components here
 export { default as BlueComments } from './comments.svelte';
-import BlueComments from './comments.svelte';
-
-customElements.define('blue-comments', BlueComments.element);
